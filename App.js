fd05ed1cecdc7a71775de3356193e770b38162e9
jeon1232 <wjsvlfdnjs@gmail.com>
2025-04-03 22:05:30 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import AdminPanel from "./AdminPanel";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <AdminPanel />
+    </div>
+  );
+}
